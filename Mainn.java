82797e33a095b190a07d906610717dded49d9d56
DeTribe <Stepoverangstrum@gmail.com>
2025-11-10 12:07:56 -0800

    push

---
+++ Mainn.java
@@ -0,0 +1,15 @@
+class Mainn{
+
+public static void sayAdios() {
+    System.out.println("Chow");
+}
+
+public static void main(String[] args) {
+    sayHello();
+    sayAdios();
+}
+
+public static void sayHello() {
+    System.out.println("Greetings");
+}
+}
