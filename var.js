2daa4961fef6848b8d8222a82128b836fd464994
DeTribe <Stepoverangstrum@gmail.com>
2025-11-19 18:02:31 -0800

    basic declaration/assignment in js

---
+++ var.js
@@ -0,0 +1,33 @@
+var num;
+num = 3;
+
+var name;
+name = 'Adam';
+
+var $firstName = "Jennifer";
+
+var _carSpecs = {
+         numWheels: 4,
+         engine: 'V6',
+         interior: 'cloth'
+};
+
+var arr123;
+
+var b;
+console.log(b); //b is undefined
+
+var a = 23;
+
+var b = a; // a and b have the same value
+
+a = 25;
+
+// b is now 23
+
+console.log(b);
+
+b="Adam"; //b is now Adam
+console.log(b);
+
+b=null; // b is now null
